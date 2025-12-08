9583458c45f6928b07232bfae63ba1b3e0bb8488
Willian Daniel Silva <137330299+daniel02211@users.noreply.github.com>
2025-12-08 15:23:43 -0300

    instalando bibliotecas

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "sonner";
 import Footer from "./_components/footer";
-import NextAuthProvider from "./_providers/session-provider";
+import NextAuthProvider from "./_providers/auth";
 
 const inter = Inter({ subsets: ["latin"] });
 
